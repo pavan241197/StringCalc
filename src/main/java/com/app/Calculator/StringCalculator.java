2840d51c57391ba7d9ceedc2b6273761d4e6b707
kumar <kumar@pavan>
2021-01-25 00:10:40 +0530

    Step 1,2

--- src/main/java/com/app/Calculator/StringCalculator.java
+++ src/main/java/com/app/Calculator/StringCalculator.java
@@ -1,5 +1,18 @@
 package com.app.Calculator;
 
 public class StringCalculator {
+	
+	public static int add(String numbers) {
+		if (numbers.equals(""))
+			return (Integer) 0;
+		else {
+			String[] numberss = numbers.split(",");
+			int sum = 0;
+			for(int i =0;i<numberss.length;i++) {
+				sum = sum+Integer.parseInt(numberss[i]);
+			}
+			return sum;
+		}
+	}
 
 }
--- src/test/java/com/app/Calculator/TestCalculator.java
+++ src/test/java/com/app/Calculator/TestCalculator.java
@@ -1,5 +1,25 @@
 package com.app.Calculator;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestCalculator {
+	private StringCalculator stringCalculator = new StringCalculator();
+
+	@Test
+	public void addEmptyString() {
+		Assert.assertEquals(0, StringCalculator.add(""));
+	}
+
+	@Test
+	public void addOneNumberShouldReturnTheSameNumberAsInteger() {
+		Assert.assertEquals(10, StringCalculator.add("10"));
+		Assert.assertEquals(20, StringCalculator.add("20"));
+	}
+
+	@Test
+	public void addTwoNumbersShouldReturnTheSum() {
+		Assert.assertEquals(11, StringCalculator.add("5,6"));
+	}
 
 }
